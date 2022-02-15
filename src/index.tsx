c58596dd74f1f63325773c6773991afbec496d25
Guilherme Correa <guilhermescorrea@hotmail.com.br>
2022-02-15 16:10:56 -0300

    create post route miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,48 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'dev',
+          amount: 100,
+          type: 'deposit',
+          category: 'freela',
+          createdAt: new Date('2021-02-01 10:00:00')
+        },
+        {
+          id: 2,
+          title: 'agua',
+          amount: 200,
+          type: 'withdraw',
+          category: 'contas a pagar',
+          createdAt: new Date('2021-02-05 15:00:00')
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const transaction = JSON.parse(request.requestBody);
+      transaction.createdAt = new Date();
+      return schema.create('transaction', transaction);
     })
   }
 })
